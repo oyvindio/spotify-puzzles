4152f682e2c2df48b13c68105ab75f920f59ccf3
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2014-04-21 15:30:20 +0200

    Add zipfs_song

---
+++ test_zipfs_song.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import division
+
+from zipfs_song import song_counts
+from zipfs_song import quality
+from zipfs_song import zipf_index
+from zipfs_song import to_songs
+from zipfs_song import sorted_songs
+from zipfs_song import Song
+
+def test_song_counts():
+    assert song_counts('4 2') == (4, 2)
+
+def test_song_counts_with_whitespace():
+    assert song_counts(' 4 2  ') == (4, 2)
+
+def test_to_songs():
+    song = to_songs(['4 foo'])[0]
+    assert song.index == 1
+    assert song.times_played == 4
+    assert song.title == 'foo'
+
+def test_zipf_index():
+    song1, song2 = to_songs(['4 foo', '6 bar'])
+    assert zipf_index(song1) == 1 / 1
+    assert zipf_index(song2) == 1 / 2
+
+def test_quality():
+    song1, song2 = to_songs(['4 foo', '6 bar'])
+    assert quality(song1) == 4 / (1 / 1)
+    assert quality(song2) == 6 / (1 / 2)
+
+def test_sorted_songs():
+    songs = to_songs(['4 foo', '6 bar', '10 baz'])
+    expected = [Song(3, 10, 'baz'), Song(2, 6, 'bar'), Song(1, 4, 'foo')]
+    assert quality(expected[0]) > quality(expected[1]) > quality(expected[2])
+    assert sorted_songs(songs) == expected
---
+++ zipfs_song.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import division
+import itertools
+import fileinput
+from collections import namedtuple
+
+Song = namedtuple('Song', ['index', 'times_played', 'title'])
+
+def zipf_index(song):
+    return 1 / song.index
+
+def quality(song):
+    return song.times_played / zipf_index(song)
+
+def sorted_songs(songs):
+    songs_with_quality_sorted_by_quality = sorted(songs_with_quality(songs), key=lambda e: e[0], reverse=True)
+    return [song_with_quality[1] for song_with_quality in songs_with_quality_sorted_by_quality]
+
+
+def songs_with_quality(songs):
+    return [(quality(song), song) for song in songs]
+
+def song_counts(line):
+    songs_given, songs_to_select = (int(part) for part in line.strip().split(' '))
+    return songs_given, songs_to_select
+
+def to_songs(lines):
+    def parse_line(line):
+        parts = line.strip().split(' ')
+        return int(parts[0]), parts[1]
+    return [Song(index + 1, *parse_line(line)) for index, line in enumerate(lines)]
+    
+if __name__ == '__main__':
+    input_lines = fileinput.input()
+    songs_given, songs_to_select = song_counts(''.join(itertools.islice(input_lines, 1)))
+    songs = to_songs(input_lines)
+    songs_sorted_by_quality = sorted_songs(songs)
+    selected_songs = itertools.islice(songs_sorted_by_quality, songs_to_select)
+    for song in selected_songs:
+        print(song.title)
